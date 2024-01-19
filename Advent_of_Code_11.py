23d73ac062d5ced60e148621e89be6cbe5042354
David Mandak <dejf.mandak@gmail.com>
2024-01-19 16:28:19 -0600

    11 done

--- Advent_of_Code_11.py
+++ Advent_of_Code_11.py
@@ -28,5 +28,23 @@ for galaxy in galaxies:
     for j in range(i, len(galaxies)):
         destination = galaxies[j]
         total += abs(galaxy[0]-destination[0])+abs(galaxy[1]-destination[1])
+        if galaxy[0] < destination[0]:
+            x_start = galaxy[0]+1
+            x_end = destination[0]
+        else:
+            x_start = destination[0]+1
+            x_end = galaxy[0]
+        for x in range(x_start, x_end):
+            if x in x_empty:
+                total += 999999
+        if galaxy[1] < destination[1]:
+            y_start = galaxy[1]+1
+            y_end = destination[1]
+        else:
+            y_start = destination[1]+1
+            y_end = galaxy[1]
+        for y in range(y_start, y_end):
+            if y in y_empty:
+                total += 999999
     i += 1
 print(total)
