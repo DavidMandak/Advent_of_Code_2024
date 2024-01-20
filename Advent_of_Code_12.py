19ce1b1896df220f3fc80f65b97cb4eeefea5b4f
David Mandak <dejf.mandak@gmail.com>
2024-01-20 11:16:02 -0600

    start of 12

--- Advent_of_Code_12.py
+++ Advent_of_Code_12.py
@@ -9,6 +9,41 @@ for line in lines:
     for i in range(0, len(record)):
         record[i] = int(record[i])
     records.append(record)
+
+
+def generate(j, place, row):
+    if place == count:
+        print(row)
+        check(row)
+    else:
+        store = row
+        for i in range(j, len(unknown)-count+1+place):
+            row = store
+            index = unknown[i]
+            row = row[:index] + "#" + row[index+1:]
+            generate(i+1, place+1, row)
+
+
+def check(row):
+    global total
+    a = 0
+    sequence = None
+    checking = False
+    for spring in row:
+        if checking is True:
+            if spring == "#":
+                sequence -= 1
+            elif sequence == 0:
+                checking = False
+                a += 1
+            else:
+                return
+        elif spring == "#":
+            checking = True
+            sequence = records[0][a]-1
+    total += 1
+
+
 row = springs[0]
 count = 0
 for num in records[0]:
@@ -19,33 +54,9 @@ for i in range(0, len(row)):
         count -= 1
     elif row[i] == "?":
         unknown.append(i)
-
-
-def generate(j, place, row):
-    if place == count:
-        check(row)
-    else:
-        store = row
-        for i in range(j, len(unknown)-count+1+place):
-            row = store
-            index = unknown[i]
-            row = row[:index] + "#" + row[index + 1:]
-            generate(i+1, place+1, row)
-
-
-def check(row):
-    global total
-    checking = False
-    for spring in row:
-        if checking is True:
-            ...
-        elif spring == "#":
-            ...
-
-
-
 for i in range(0, len(unknown)-count+1):
-    row = row[0]
+    row = springs[0]
     index = unknown[i]
     row = row[:index]+"#"+row[index+1:]
     generate(i+1, 1, row)
+print(total)
