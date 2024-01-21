e9f6a0cec4341f52033c93c7a3d67ccbf195203a
David Mandak <dejf.mandak@gmail.com>
2024-01-21 17:27:13 -0600

    Works, but is way too slow

--- Advent_of_Code_12.py
+++ Advent_of_Code_12.py
@@ -4,8 +4,8 @@ records = []
 total = 0
 for line in lines:
     line = line.split(" ")
-    springs.append(line[0])
-    record = line[1].split(",")
+    springs.append(((line[0]+"?")*5)[:-1])
+    record = line[1].split(",")*5
     for i in range(0, len(record)):
         record[i] = int(record[i])
     records.append(record)
@@ -19,8 +19,31 @@ def generate(j, place, row, record):
         for i in range(j, len(unknown)-count+1+place):
             row = store
             index = unknown[i]
-            row = row[:index] + "#" + row[index+1:]
+            row = row[:index]+"#"+row[index+1:]
+            if memoization(row, index, record) is False:
+                continue
             generate(i+1, place+1, row, record)
+
+
+def memoization(row, index, record):
+    a = 0
+    sequence = None
+    checking = False
+    for i in range(0, index+1):
+        spring = row[i]
+        if checking is True:
+            if spring == "#":
+                sequence -= 1
+            elif sequence == 0:
+                checking = False
+                a += 1
+            else:
+                return False
+        elif spring == "#":
+            checking = True
+            sequence = record[a]-1
+    if sequence < 0:
+        return False
 
 
 def check(row, record):
@@ -33,6 +56,8 @@ def check(row, record):
         if checking is True:
             if spring == "#":
                 sequence -= 1
+            elif sequence < 0:
+                return
             elif sequence == 0:
                 checking = False
                 a += 1
@@ -56,4 +81,5 @@ for i in range(0, len(lines)):
         elif row[j] == "?":
             unknown.append(j)
     generate(0, 0, springs[i], records[i])
+    break
 print(total)
