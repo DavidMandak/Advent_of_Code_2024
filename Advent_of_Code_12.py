79ed1ce627ace780e76d6349aea6a9277a5e75c4
David Mandak <dejf.mandak@gmail.com>
2024-01-29 16:43:45 -0600

    Works considerably faster, but is still pretty slow.

--- Advent_of_Code_12.py
+++ Advent_of_Code_12.py
@@ -2,6 +2,7 @@ lines = open("Advent_of_Code_12.txt").read().splitlines()
 springs = []
 records = []
 total = 0
+transfer = None
 for line in lines:
     line = line.split(" ")
     springs.append(((line[0]+"?")*5)[:-1])
@@ -11,39 +12,45 @@ for line in lines:
     records.append(record)
 
 
-def generate(j, place, row, record):
+def generate(location_index, place, row, record, record_index, sequence, checking):
+    global transfer, total
     if place == count:
-        check(row, record)
+        if memoization(row, len(row)-1, record, record_index, sequence, checking, location_index) is not False:
+            total += 1
     else:
-        store = row
-        for i in range(j, len(unknown)-count+1+place):
-            row = store
-            index = unknown[i]
-            row = row[:index]+"#"+row[index+1:]
-            if memoization(row, index, record) is False:
+        save = row
+        for i in range(location_index+1, len(unknown)-count+1+place):
+            row = save
+            location = unknown[i]
+            row = row[:location]+"#"+row[location+1:]
+            transfer = None
+            if memoization(row, location, record, record_index, sequence, checking, location_index) is False:
                 continue
-            generate(i+1, place+1, row, record)
+            generate(i, place+1, row, record, transfer[0], transfer[1], transfer[2])
 
 
-def memoization(row, index, record):
-    a = 0
-    sequence = None
-    checking = False
-    for i in range(0, index+1):
+def memoization(row, location, record, record_index, sequence, checking, location_index):
+    global transfer
+    if location_index == -1:
+        start = 0
+    else:
+        start = unknown[location_index]+1
+    for i in range(start, location+1):
         spring = row[i]
         if checking is True:
             if spring == "#":
                 sequence -= 1
             elif sequence == 0:
                 checking = False
-                a += 1
+                record_index += 1
             else:
                 return False
         elif spring == "#":
             checking = True
-            sequence = record[a]-1
+            sequence = record[record_index]-1
     if sequence < 0:
         return False
+    transfer = [record_index, sequence, checking]
 
 
 def check(row, record):
@@ -67,6 +74,7 @@ def check(row, record):
             checking = True
             sequence = record[a]-1
     total += 1
+    return True
 
 
 for i in range(0, len(lines)):
@@ -80,6 +88,6 @@ for i in range(0, len(lines)):
             count -= 1
         elif row[j] == "?":
             unknown.append(j)
-    generate(0, 0, springs[i], records[i])
+    generate(-1, 0, springs[i], records[i], 0, None, False)
     break
 print(total)
