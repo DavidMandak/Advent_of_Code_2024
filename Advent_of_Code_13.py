91ecf4e0dff19a7be88924aadfc04f94890e7b68
David Mandak <dejf.mandak@gmail.com>
2024-02-13 16:06:12 -0600

    Part 2 done

--- Advent_of_Code_13.py
+++ Advent_of_Code_13.py
@@ -1,8 +1,5 @@
 patterns = open("Advent_of_Code_13.txt").read().split("\n\n")
-practise = ["#.##..##.\n..#.##.#.\n##......#\n##......#\n..#.##.#.\n..##..##.\n#.#.##.#.",
-            "#...##..#\n#....#..#\n..##..###\n#####.##.\n#####.##.\n..##..###\n#....#..#"
-]
-
+practise = open("test.txt").read().split("\n\n")
 vertical = 0
 horizontal = 0
 
@@ -11,22 +8,54 @@ def search(grid, axis):
     for i in range(1, len(grid)):
         if grid[i] == grid[i-1]:
             if len(grid)-i < i:
-                if check(i, len(grid)-i, grid, axis) is not False:
+                if check(i, len(grid) - i, grid, axis, 0) is not False:
                     return True
             else:
-                if check(i, i, grid, axis) is not False:
+                if check(i, i, grid, axis, 0) is not False:
                     return True
+        else:
+            line_1 = grid[i]
+            line_2 = grid[i-1]
+            found = 0
+            for j in range(0, len(line_1)):
+                if line_1[j] != line_2[j]:
+                    if found == 0:
+                        found += 1
+                    else:
+                        found += 1
+                        break
+            if found == 1:
+                if len(grid) - i < i:
+                    if check(i, len(grid) - i, grid, axis, found) is not False:
+                        return True
+                else:
+                    if check(i, i, grid, axis, found) is not False:
+                        return True
 
 
-def check(i, size, grid, axis):
+def check(i, size, grid, axis, count):
     global horizontal, vertical
-    for y in range(1, size+1):
+    for y in range(2, size+1):
         if grid[i+y-1] != grid[i-y]:
-            return False
-    if axis == "y":
-        vertical += i
+            if count != 0:
+                return False
+            line_1 = grid[i+y-1]
+            line_2 = grid[i-y]
+            found = False
+            for j in range(0, len(line_1)):
+                if line_1[j] != line_2[j]:
+                    if found is False:
+                        found = True
+                    else:
+                        return False
+            count += 1
+    if count == 1:
+        if axis == "y":
+            vertical += i
+        else:
+            horizontal += i
     else:
-        horizontal += i
+        return False
 
 
 for pattern in patterns:
