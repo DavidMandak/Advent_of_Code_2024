d98e11db32bab19ba4cea44e7ebeedf20a9478fb
DavidMandak <dejf.mandak@gmail.com>
2024-12-01 10:13:14 +0100

    Idk

--- Advent_of_Code_17/Advent_of_Code_17.py
+++ Advent_of_Code_17/Advent_of_Code_17.py
@@ -1,5 +1,5 @@
-lines1 = open("Advent_of_Code_17.txt").read().splitlines()
-lines = open("../test.txt").read().splitlines()
+lines = open("Advent_of_Code_17.txt").read().splitlines()
+lines1 = open("../test.txt").read().splitlines()
 values = []
 huh = []
 for y in range(0, len(lines)):
@@ -18,28 +18,28 @@ def f(x, y, value, count, direction):
     else:
         return
     value += int(lines[y][x])
-    if values[y][x] is None or value < values[y][x]:
+    if values[y][x] is None or value < values[y][x] or (value-5 < values[y][x] and (huh[y][x][0] != direction or huh[y][x][1] > count)):
         values[y][x] = value
         if direction == "R":
-            huh[y][x] = ">"
+            huh[y][x] = ("R", count)
             if count < 3:
                 next_check.append((x+1, y, value, count+1, "R"))
             next_check.append((x, y-1, value, 1, "U"))
             next_check.append((x, y+1, value, 1, "D"))
         elif direction == "L":
-            huh[y][x] = "<"
+            huh[y][x] = ("L", count)
             if count < 3:
                 next_check.append((x-1, y, value, count+1, "L"))
             next_check.append((x, y-1, value, 1, "U"))
             next_check.append((x, y+1, value, 1, "D"))
         elif direction == "U":
-            huh[y][x] = "^"
+            huh[y][x] = ("U", count)
             if count < 3:
                 next_check.append((x, y-1, value, count+1, "U"))
             next_check.append((x+1, y, value, 1, "R"))
             next_check.append((x-1, y, value, 1, "L"))
         else:
-            huh[y][x] = "v"
+            huh[y][x] = ("D", count)
             if count < 3:
                 next_check.append((x, y+1, value, count+1, "D"))
             next_check.append((x+1, y, value, 1, "R"))
@@ -53,20 +53,23 @@ while len(next_check) > 0:
         f(pos[0], pos[1], pos[2], pos[3], pos[4])
 print(values[-1][-1]-int(lines[0][0]))
 x, y = (len(huh[-1])-1, len(huh)-1)
+
 while (x, y) != (0, 0):
-    if huh[y][x] == ">":
-        huh[y][x] = "#"
+    if huh[y][x][0] == "R":
+        huh[y][x] = ("#")
         x -= 1
-    elif huh[y][x] == "<":
-        huh[y][x] = "#"
+    elif huh[y][x][0] == "L":
+        huh[y][x] = ("#")
         x += 1
-    elif huh[y][x] == "^":
-        huh[y][x] = "#"
+    elif huh[y][x][0] == "U":
+        huh[y][x] = ("#")
         y += 1
-    elif huh[y][x] == "v":
-        huh[y][x] = "#"
+    elif huh[y][x][0] == "D":
+        huh[y][x] = ("#")
         y -= 1
 for line in huh:
     for tile in line:
-        print(tile, end="")
+        print(tile[0], end="")
     print()
+print()
+print(values)
