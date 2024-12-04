e4ad2271e65160ef92bb4508285d1d886016702e
DavidMandak <dejf.mandak@gmail.com>
2024-12-04 17:16:01 +0100

    Day 4 done

--- Programs/Advent_of_Code_04.py
+++ Programs/Advent_of_Code_04.py
@@ -1,21 +1,32 @@
-lines = open("../Inputs/test.txt").read().splitlines()
+lines = open("../Inputs/Advent_of_Code_04.txt").read().splitlines()
 
 
 def check(x, y, string):
   global total
-  if lines[y][x:x+4] == string:
-    total += 1
   for a, b in directions:
-    if all(lines[y+i*a][x+i*b] == string[i] for i in range(1, 4)):
-      total += 1
+    if 0 <= x+3*b < len(lines[0]) and 0 <= y+3*a < len(lines) and all(lines[y+i*a][x+i*b] == string[i]
+                                                                        for i in range(1, 4)):
+        total += 1
 
-
-directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]
 total = 0
+directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]
 for y in range(len(lines)):
-  for x in range(len(lines[0]))
+  for x in range(len(lines[0])):
     if lines[y][x] == "X":
       check(x, y, "XMAS")
     elif lines[y][x] == "S":
       check(x, y, "SAMX")
 print(total)
+
+total = 0
+directions = [((-1, -1), (1, 1)), ((-1, 1), (1, -1))]
+for y in range(1, len(lines)-1):
+  for x in range(1, len(lines[0])-1):
+    if lines[y][x] == "A":
+      for a, b in directions:
+        if lines[y+a[0]][x+a[1]] == "M" and lines[y+b[0]][x+b[1]] == "S":
+          total += 1/2
+        elif lines[y+a[0]][x+a[1]] == "S" and lines[y+b[0]][x+b[1]] == "M":
+          total += 1/2
+      total //= 1
+print(int(total))
--- Programs/test.py
+++
@@ -1,4 +0,0 @@
-a = "mul(1, 2)"
-print(a[0:20])
-b = 4
-print(a[b])
