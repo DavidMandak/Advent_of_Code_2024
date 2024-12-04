53f334e0f43e82b0b1af276a8fd881950c55086d
DavidMandak <116176892+DavidMandak@users.noreply.github.com>
2024-12-04 15:05:02 +0100

    Probably works

--- Programs/Advent_of_Code_04.py
+++ Programs/Advent_of_Code_04.py
@@ -1,7 +1,21 @@
-lines = open("../Inputs/test.txt).read().splitlines()
+lines = open("../Inputs/test.txt").read().splitlines()
 
+
+def check(x, y, string):
+  global total
+  if lines[y][x:x+4] == string:
+    total += 1
+  for a, b in directions:
+    if all(lines[y+i*a][x+i*a] == string[i] for i in range(1, 4)):
+      total += 1
+
+
+directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]
 total = 0
 for y in range(len(lines)):
   for x in range(len(lines[0]))
     if lines[y][x] == "X":
-      
+      check(x, y, "XMAS")
+    elif lines[y][x] == "S":
+      check(x, y, "SAMX")
+print(total)
