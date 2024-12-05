946c1fbb0372951a8fab67aa72faf98cf311c7c7
DavidMandak <dejf.mandak@gmail.com>
2024-12-05 14:13:21 +0100

    idk

--- Programs/Advent_of_Code_05.py
+++ Programs/Advent_of_Code_05.py
@@ -1,12 +1,19 @@
 from collections import defaultdict
-input = open("../Inputs/Advent_of_Code_05").read().splitlines().split("")
+lines = open("../Inputs/test.txt").read().split("\n\n")
 
 rules = defaultdict(list)
-pairs = input[0]
-updates = input[1]
+pairs = lines[0].splitlines()
+updates = lines[1].splitlines()
+for pair in pairs:
+    a, b = map(int, pair.split("|"))
+    rules[b].append(a)
 
-for a, b in map(int, pairs.split("|")):
-  rules[b].append(a)
-
+total = 0
 for update in updates:
-  ...
+    line = map(int, update.split(","))
+    check = [line[0]]
+    for num in line[1:]:
+        for x in rules[num]:
+            if x in check:
+              total -= 1
+              break
