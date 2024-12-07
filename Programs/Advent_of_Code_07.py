60f33c69f95ddd198677f225815255a202311c85
David Mandak <dejf.mandak@gmail.com>
2024-12-07 23:24:31 +0100

    solution for both day 7

--- Programs/Advent_of_Code_07.py
+++ Programs/Advent_of_Code_07.py
@@ -7,7 +7,7 @@ def f(pos, value):
         if value == result:
             total += result
             return True
-    elif f(pos+1, value*numbers[pos]) or f(pos+1, value+numbers[pos]) or f(pos+1, int(str(value)+str(numbers[pos]))):
+    elif f(pos+1, value*numbers[pos]) or f(pos+1, value+numbers[pos]):
         return True
 
 
@@ -17,4 +17,23 @@ for line in lines:
     result = int(line[0])
     numbers = list(map(int, line[1].split()))
     f(1, numbers[0])
+print(total)
+
+
+def g(pos, value):
+    global total
+    if pos == len(numbers):
+        if value == result:
+            total += result
+            return True
+    elif g(pos+1, value*numbers[pos]) or g(pos+1, value+numbers[pos]) or g(pos+1, int(str(value)+str(numbers[pos]))):
+        return True
+
+
+total = 0
+for line in lines:
+    line = line.split(": ")
+    result = int(line[0])
+    numbers = list(map(int, line[1].split()))
+    g(1, numbers[0])
 print(total)
