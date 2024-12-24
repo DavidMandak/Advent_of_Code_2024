caddf5cc934714a0844d2fde7782a7e15f01f3ac
DavidMandak <dejf.mandak@gmail.com>
2024-12-24 23:57:11 +0100

    Day 21 pat 2 done

--- Programs/Advent_of_Code_21.py
+++ Programs/Advent_of_Code_21.py
@@ -7,7 +7,9 @@ pad = {
     "0,-1":(1, 0), "0,0":(2, 0),
     "-1,0":(0, 1), "0,1":(1, 1), "1,0":(2, 1)
 }
+
 total = 0
+segments = []
 for line in lines:
     px, py = 2, 3
     code = line
@@ -31,59 +33,54 @@ for line in lines:
                     path += f"{diff_x//abs(diff_x)},0 "*abs(diff_x)
                 path += "0,0 "
             px, py = x, y
+        if _ == 0:
+            segments.append([s+"0,0" for s in path.split("0,0 ")][:-1]) # This is for part 2
         code = path.split()
         px, py = 2, 0
     total += len(path.split())*int(line[:-1])
 print(total)
 
-memo = {depth: {} for depth in range(26)}
-total = 0
-for line in lines:
-    px, py = 2, 3
-    code = [button+" " for button in line]
-    for _ in range(26):
-        result = ""
-        ey = py
-        for seg in code:
-            if seg not in memo[_]:
-                path = ""
-                for num in seg.split():
-                    x, y = pad[num]
-                    diff_x = x-px
-                    diff_y = y-py
-                    if px == 0 and y == ey or (diff_x < 0 and (x != 0 or py != ey)):
-                        if diff_x != 0:
-                            path += f"{diff_x//abs(diff_x)},0 "*abs(diff_x)
-                        if diff_y != 0:
-                            path += f"0,{diff_y//abs(diff_y)} "*abs(diff_y)
-                        path += "0,0 "
-                    else:
-                        if diff_y != 0:
-                            path += f"0,{diff_y//abs(diff_y)} "*abs(diff_y)
-                        if diff_x != 0:
-                            path += f"{diff_x//abs(diff_x)},0 "*abs(diff_x)
-                        path += "0,0 "
-                    px, py = x, y
-                if _ != 0:
-                    memo[_][seg] = path
-                result += path
-            else:
-                result += memo[_][seg]
-        code = [seg+"0,0" for seg in result.split("0,0 ")]
-        px, py = 2, 0
-    total += (len(result.split())-2)*int(line[:-1])
-    print("done")
-print(total)
 
-for direction in result.split():
-    if direction == "1,0":
-        print(">", end="")
-    elif direction == "-1,0":
-        print("<", end="")
-    elif direction == "0,-1":
-        print("^", end="")
-    elif direction == "0,1":
-        print("v", end="")
+def move(seg, depth):
+    global cache
+    if depth == 0:
+        return len(seg.split())
+    px, py = 2, 0
+    ey = py
+    if seg not in cache[depth]:
+        path = ""
+        for num in seg.split():
+            x, y = pad[num]
+            diff_x = x-px
+            diff_y = y-py
+            if px == 0 and y == ey or (diff_x < 0 and (x != 0 or py != ey)):
+                if diff_x != 0:
+                    path += f"{diff_x // abs(diff_x)},0 " * abs(diff_x)
+                if diff_y != 0:
+                    path += f"0,{diff_y // abs(diff_y)} " * abs(diff_y)
+                path += "0,0 "
+            else:
+                if diff_y != 0:
+                    path += f"0,{diff_y // abs(diff_y)} " * abs(diff_y)
+                if diff_x != 0:
+                    path += f"{diff_x // abs(diff_x)},0 " * abs(diff_x)
+                path += "0,0 "
+            px, py = x, y
+        count = 0
+        for part in [p+"0,0" for p in path.split("0,0 ")][:-1]:
+             count += move(part, depth-1)
+        cache[depth][seg] = count
+        return count
     else:
-        print("A", end="")
-print()
+        return cache[depth][seg]
+
+
+cache = {d:{} for d in range(1, 26)}
+total = 0
+for i in range(len(lines)):
+    segment = segments[i]
+    seg_sum = 0
+    for s in segment:
+        seg_sum += move(s, 25)
+    total += seg_sum*int(lines[i][:-1])
+print(total)
--- Programs/test.py
+++ Programs/test.py
@@ -1,9 +1,6 @@
-n = 0
-length = range(222)
-for i in length:
-    for j in length:
-        for k in length:
-            for l in length:
-                n += 1
-    print(n)
-print(n)
+
+a = "1235 5 5 "
+print([b+"5" for b in a.split("5 ")])
+a = "123"
+a = a.split("3")
+print(a)
