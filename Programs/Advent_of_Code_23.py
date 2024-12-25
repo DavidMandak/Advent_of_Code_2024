463d6cb0ac8d2c30ad0b439ad8e4a6320dac059c
DavidMandak <dejf.mandak@gmail.com>
2024-12-25 10:18:21 +0100

    Day 23 pat 2 done

--- Programs/Advent_of_Code_23.py
+++ Programs/Advent_of_Code_23.py
@@ -9,10 +9,20 @@ for pair in lines:
     neighbours[b].append(a)
 groups = []
 for pc in neighbours:
-    if pc[0] == "t":
+    if pc[0] == "t": # Change to True for part 2
         for neighbour in neighbours[pc]:
             for third in neighbours[neighbour]:
                 if pc in neighbours[third] and {pc:0, neighbour:0, third:0} not in groups:
                     groups.append({pc:0, neighbour:0, third:0})
                     total += 1
 print(total)
+codes = defaultdict(int)
+for group in list(map(list, groups)):
+    for code in group:
+        codes[code] += 1
+party = []
+for code in codes:
+    if codes[code] == 66:
+        party.append(code)
+party.sort()
+print(str(party)[2:-2].replace("\'", "").replace(" ", ""))
